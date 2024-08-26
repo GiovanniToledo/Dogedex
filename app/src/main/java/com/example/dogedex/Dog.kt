d90ec0f1c0565b909e77447d40efa921c8bb3f7d
Giovanni Toledo <giio.toledo@gmail.com>
2024-08-26 13:12:48 -0600

    first commit

---
+++ app/src/main/java/com/example/dogedex/Dog.kt
@@ -0,0 +1,17 @@
+package com.example.dogedex
+
+class Dog(
+    val id: Int,
+    val index: Int,
+    val name: String,
+    val type: String,
+    val heightFemale: Double,
+    val heightMale: Double,
+    val imageUrl: String,
+    val lifeExpectancy: String,
+    val temperament: String,
+    val weightFemale: Double,
+    val weightMale: Double
+) {
+
+}
